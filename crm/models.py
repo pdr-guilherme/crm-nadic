ea873b8d03a1d8c3fce26b2f44f2ba5b6b7e3995
Pedro Guilherme <pedro.guilherme.dsa@gmail.com>
2025-03-06 20:40:45 -0300

    Commit inicial

---
+++ crm/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Produto(models.Model):
+    pass
+
+
+class Estoque(models.Model):
+    pass
+
+
+class Faturamento(models.Model):
+    pass
+
+
+class Cliente(models.Model):
+    pass
