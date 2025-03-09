73303fd28370f525afe2db074af2c7e4fc186957
Pedro Guilherme <pedro.guilherme.dsa@gmail.com>
2025-03-09 20:14:50 -0300

    Criar primeiros modelos

--- crm/admin.py
+++ crm/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Produto, Estoque
 
-# Register your models here.
+
+class ProdutoAdmin(admin.ModelAdmin):
+    fields = ("nome", "descricao", "preco", "categoria", "ativo")
+    list_display = ("nome", "preco", "categoria", "ativo")
+    list_filter = ("ativo",)
+
+
+class EstoqueAdmin(admin.ModelAdmin):
+    fields = ("produto", "quantia")
+    list_display = ("produto", "quantia")
+
+
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Estoque, EstoqueAdmin)
--- crm/models.py
+++ crm/models.py
@@ -1,12 +1,44 @@
 from django.db import models
 
 
+# tipo de loja: floricultura (possivelmente)
+
 class Produto(models.Model):
-    pass
+    """Modelo que representa um produto na loja."""
+    CATEGORIA_CHOICES = {
+        "plantas": "Plantas",
+        "vasos": "Vasos",
+        "flores": "Flores",
+        "buquê": "Buquê de Flores",
+        "arranjo": "Arranjo de Flores",
+        "cesta": "Cesta de Presente",
+    }
+
+    nome = models.CharField("Nome do produto", max_length=100)
+    descricao = models.CharField("Descrição do produto", max_length=255)
+    preco = models.DecimalField("Preço do produto", max_digits=7, decimal_places=2)
+    ativo = models.BooleanField("Produto ativo", default=True)
+    data_criacao = models.DateField("Data de criação", auto_now_add=True)
+    categoria = models.CharField("Categoria do produto", max_length=17, choices=CATEGORIA_CHOICES)
+
+    def __str__(self) -> str:
+        return f"{self.nome} (R$ {self.preco}): {self.descricao}"
+
+    class Meta:
+        verbose_name = "Produto"
+        verbose_name_plural = "Produtos"
+        ordering = ["nome",]
 
 
 class Estoque(models.Model):
-    pass
+    """
+    Classe responsável por gerenciar a quantidade de produtos disponíveis no estoque.
+    """
+    produto = models.OneToOneField(Produto, on_delete=models.CASCADE, related_name="estoque")
+    quantia = models.PositiveIntegerField("Quantidade em estoque", blank=True, default=0)
+
+    def __str__(self) -> str:
+        return f"Produto {self.produto} - {self.quantia} disponíveis em estoque"
 
 
 class Faturamento(models.Model):
