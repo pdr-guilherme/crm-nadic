8cf38fc4f4635e85ef9dbc007194ec4b88b6e21d
Pedro Guilherme <pedro.guilherme.dsa@gmail.com>
2025-03-12 17:06:10 -0300

    Criar view de listagem para Produto

--- crm/views.py
+++ crm/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Produto
+
+
+class ProdutoList(generic.ListView):
+    model = Produto
+    context_object_name = "produtos"
+    template_name = "crm/listar_produtos.html"
