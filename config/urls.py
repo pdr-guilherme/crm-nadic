2fa729f3af1f99295a5b89116a4186630a0e1bef
Pedro Guilherme <pedro.guilherme.dsa@gmail.com>
2025-03-30 17:27:45 -0300

    Refatorar urls

--- config/urls.py
+++ config/urls.py
@@ -16,34 +16,54 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from crm import views
+
+produto_urls = [
+    path("", views.ProdutoList.as_view(), name="produtos"),
+    path("novo/", views.ProdutoCreateForm.as_view(), name="criar_produto"),
+    path("<int:pk>/editar/", views.ProdutoUpdate.as_view(), name="editar_produto"),
+    path("<int:pk>/apagar/", views.apagar_produto, name="apagar_produto"),
+]
+
+estoque_urls = [
+    path("", views.EstoqueList.as_view(), name="estoque"),
+    path("novo/", views.EstoqueCreateForm.as_view(), name="criar_estoque"),
+    path("<int:pk>/editar/", views.EstoqueUpdate.as_view(), name="editar_estoque"),
+    path("<int:pk>/apagar/", views.apagar_estoque, name="apagar_estoque"),
+]
+
+cliente_urls = [
+    path("", views.ClienteList.as_view(), name="clientes"),
+    path("novo/", views.ClienteFormCreate.as_view(), name="criar_cliente"),
+    path("<int:pk>/editar/", views.ClienteUpdate.as_view(), name="editar_cliente"),
+    path("<int:pk>/apagar/", views.apagar_cliente, name="apagar_cliente"),
+]
+
+lead_urls = [
+    path("", views.LeadList.as_view(), name="leads"),
+    path("novo/", views.LeadFormCreate.as_view(), name="criar_lead"),
+    path("<int:pk>/editar/", views.LeadUpdate.as_view(), name="editar_lead"),
+    path("<int:pk>/apagar/", views.apagar_lead, name="apagar_lead"),
+    path(
+        "<int:pk>/converter/", views.ConverterLeadView.as_view(), name="converter_lead"
+    ),
+]
+
+venda_urls = [
+    path("", views.VendaList.as_view(), name="vendas"),
+    path("novo/", views.VendaFormCreate.as_view(), name="criar_venda"),
+    path("<int:pk>/editar/", views.VendaUpdate.as_view(), name="editar_venda"),
+    path("<int:pk>/apagar/", views.apagar_venda, name="apagar_venda"),
+]
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.IndexView.as_view(), name="index"),
-    path("produtos/", views.ProdutoList.as_view(), name="produtos"),
-    path("produtos/novo/", views.ProdutoCreateForm.as_view(), name="criar_produto"),
-    path(
-        "produto/<int:pk>/editar/", views.ProdutoUpdate.as_view(), name="editar_produto"
-    ),
-    path("produto/<int:pk>/apagar/", views.apagar_produto, name="apagar_produto"),
-    path("estoque/", views.EstoqueList.as_view(), name="estoque"),
-    path("estoque/novo/", views.EstoqueCreateForm.as_view(), name="criar_estoque"),
-    path("estoque/<int:pk>/editar/", views.EstoqueUpdate.as_view(), name="editar_estoque"),
-    path("estoque/<int:pk>/apagar/", views.apagar_estoque, name="apagar_estoque"),
-    path("clientes/", views.ClienteList.as_view(), name="clientes"),
-    path("clientes/novo/", views.ClienteFormCreate.as_view(), name="criar_cliente"),
-    path("clientes/<int:pk>/editar/", views.ClienteUpdate.as_view(), name="editar_cliente"),
-    path("clientes/<int:pk>/apagar/", views.apagar_cliente, name="apagar_cliente"),
-    path("leads/", views.LeadList.as_view(), name="leads"),
-    path("leads/novo/", views.LeadFormCreate.as_view(), name="criar_lead"),
-    path("leads/<int:pk>/editar/", views.LeadUpdate.as_view(), name="editar_lead"),
-    path("leads/<int:pk>/apagar/", views.apagar_lead, name="apagar_lead"),
-    path("leads/<int:pk>/converter/", views.ConverterLeadView.as_view(), name="converter_lead"),
-    path("vendas/", views.VendaList.as_view(), name="vendas"),
-    path("vendas/novo/", views.VendaFormCreate.as_view(), name="criar_venda"),
-    path("vendas/<int:pk>/editar/", views.VendaUpdate.as_view(), name="editar_venda"),
-    path("vendas/<int:pk>/apagar/", views.apagar_venda, name="apagar_venda")
+    path("produtos/", include(produto_urls)),
+    path("estoque/", include(estoque_urls)),
+    path("clientes/", include(cliente_urls)),
+    path("leads/", include(lead_urls)),
+    path("vendas/", include(venda_urls)),
 ]
