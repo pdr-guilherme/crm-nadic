00b99a1a0e84e46b2b7b82b2462da79d6a2e12b9
Pedro Guilherme <pedro.guilherme.dsa@gmail.com>
2025-04-01 21:19:09 -0300

    Customizar formulários

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "crm.apps.CrmConfig"
+    "crm.apps.CrmConfig",
+    "crispy_forms",
+    "crispy_bootstrap5"
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# customização de forms
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
--- crm/forms.py
+++ crm/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Fieldset, Row, Column, ButtonHolder, Submit
+
 from .models import Estoque, Lead, Produto, Cliente, Venda
 
 
@@ -8,17 +11,103 @@ class ProdutoForm(forms.ModelForm):
         model = Produto
         fields = "__all__"
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        self.helper.form_method = "POST"
+        self.helper.form_class = "form-horizontal"
+        self.helper.label_class = "col-md-4"  # tamanho do label
+        self.helper.field_class = "col-md-8"  # tamanho do campo
+        self.helper.layout = Layout(
+            Fieldset(
+                "Informações do Produto",
+                Row(
+                    Column("nome", css_class="form-group col-md-6"),
+                    Column("preco", css_class="form-group col-md-6"),
+                    css_class="form-row",
+                ),
+                "descricao",
+                Row(
+                    Column("categoria", css_class="form-group col-md-6"),
+                    Column("ativo", css_class="form-group col-md-6"),
+                    css_class="form-row",
+                ),
+            ),
+            ButtonHolder(Submit("submit", "Salvar", css_class="btn btn-primary w-100")),
+        )
+
 
 class EstoqueForm(forms.ModelForm):
     class Meta:
         model = Estoque
         fields = "__all__"
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        self.helper.form_method = "POST"
+        self.helper.form_class = "form-horizontal"
+        self.helper.label_class = "col-md-4"
+        self.helper.field_class = "col-md-8"
+        self.helper.layout = Layout(
+            Fieldset(
+                "Informações do estoque",
+                Row(
+                    Column("produto", css_class="form-group col-md-6"),
+                    Column("quantia", css_class="form-group col-md-6"),
+                    css_class="form-row",
+                ),
+            ),
+            ButtonHolder(
+                Submit("submit", "Adicionar", css_class="btn btn-primary w-100")
+            ),
+        )
+
 
 class ClienteForm(forms.ModelForm):
     class Meta:
         model = Cliente
         fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        self.helper.form_method = "POST"
+        self.helper.form_class = "form-horizontal"
+        self.helper.label_class = "col-md-4"
+        self.helper.field_class = "col-md-8"
+        self.helper.layout = Layout(
+            Fieldset(
+                "Dados do cliente",
+                Row(
+                    Column("nome", css_class="form-group col-md-6"),
+                    css_class="form-row",
+                ),
+            ),
+            Fieldset(
+                "Informações de contato",
+                Row(
+                    Column("telefone", css_class="form-group col-md-6"),
+                    Column("email", css_class="form-group col-md-6"),
+                    Column("endereco", css_class="form-group col-md-6"),
+                    css_class="form-row",
+                ),
+            ),
+            Fieldset(
+                "Outros dados",
+                Row(
+                    Column("fonte", css_class="form-group col-md-6"),
+                    Column("status", css_class="form-group col-md-6"),
+                    Column("notas", css_class="form-group col-md-6"),
+                ),
+            ),
+            ButtonHolder(
+                Submit("submit", "Adicionar", css_class="btn btn-primary w-100 mb-3")
+            ),
+        )
 
 
 class LeadForm(forms.ModelForm):
@@ -29,11 +118,81 @@ class LeadForm(forms.ModelForm):
             "data_conversao": forms.DateTimeInput(attrs={"type": "datetime-local"}),
         }
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        self.helper.form_method = "POST"
+        self.helper.form_class = "form-horizontal"
+        self.helper.label_class = "col-md-4"
+        self.helper.field_class = "col-md-8"
+        self.helper.layout = Layout(
+            Fieldset(
+                "Dados do lead",
+                Row(
+                    Column("nome", css_class="form-group col-md-6"),
+                    css_class="form-row",
+                ),
+            ),
+            Fieldset(
+                "Informações de contato",
+                Row(
+                    Column("telefone", css_class="form-group col-md-6"),
+                    Column("email", css_class="form-group col-md-6"),
+                    Column("endereco", css_class="form-group col-md-6"),
+                    css_class="form-row",
+                ),
+            ),
+            Fieldset(
+                "Outros dados",
+                Row(
+                    Column("fonte", css_class="form-group col-md-6"),
+                    Column("status", css_class="form-group col-md-6"),
+                    Column("notas", css_class="form-group col-md-6"),
+                    Column("produto_interesse", css_class="form-group col-md-6"),
+                    Column("data_conversao", css_class="form-group col-md-6"),
+                ),
+            ),
+            ButtonHolder(
+                Submit("submit", "Adicionar", css_class="btn btn-primary w-100 mb-3")
+            ),
+        )
+
 
 class VendaForm(forms.ModelForm):
     class Meta:
         model = Venda
         fields = "__all__"
-        widgets = {
-            "produtos": forms.CheckboxSelectMultiple()
-        }
+        # widgets = {
+        #     "produtos": forms.CheckboxSelectMultiple()
+        # }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        self.helper.form_method = "POST"
+        self.helper.form_class = "form-horizontal"
+        self.helper.label_class = "col-md-4"
+        self.helper.field_class = "col-md-8"
+        self.helper.layout = Layout(
+            Fieldset(
+                "Dados da venda",
+                Row(
+                    Column("cliente", css_class="form-group col-md-6"),
+                    Column("produtos", css_class="form-group col-md-8"),
+                    css_class="form-row",
+                ),
+            ),
+            Fieldset(
+                "Outros dados",
+                Row(
+                    Column("status", css_class="form-group col-md-6"),
+                    Column("forma_pagamento", css_class="form-group col-md-6"),
+                    css_class="form-row",
+                ),
+            ),
+            ButtonHolder(
+                Submit("submit", "Adicionar", css_class="btn btn-primary w-100 mb-3")
+            ),
+        )
